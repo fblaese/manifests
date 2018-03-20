d774f8572f81ad064aca2be87bd6ea54a7f5d708
LokManSiu <bjsiu0617@gmail.com>
2018-03-20 23:04:16 +0800

    Create dirty-build.sh

---
+++ dirty-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set CCACHE
+export USE_CCACHE=1
+export CCACHE_DIR=/root/ccache/lineage
+prebuilts/misc/linux-x86/ccache/ccache -M 50G
+
+# FuckingJack
+./prebuilts/sdk/tools/jack-admin kill-server
+export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4096m"
+./prebuilts/sdk/tools/jack-admin start-server
+export LANGUAGE=C
+
+# Compile the build
+. build/envsetup.sh
+breakfast kenzo
+make -j4 bacon 2>&1 | tee build.log
+
+echo -e ""
