d6145b7bbb955b3c0fee04d398bde9bec09eb567
LokManSiu <bjsiu0617@gmail.com>
2018-03-23 22:22:10 +0800

    Update clean-build.sh

--- clean-build.sh
+++ clean-build.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# Make a clean build, building dirty after you have had jack issues may result in a failed build
-make -j4 clean && make -j4 clobber
-
 # FuckingJack
 ./prebuilts/sdk/tools/jack-admin kill-server
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4096m"
 ./prebuilts/sdk/tools/jack-admin start-server
 export LANGUAGE=C
 
-# Sign Build
-export SIGN_BUILD=true
-
-# Compile the build
+# Initialize Environment and Lunch
 . build/envsetup.sh
 breakfast kenzo
+
+# Make a clean build, building dirty after you have had jack issues may result in a failed build
+make -j4 clean && make -j4 clobber
+
+# Compile the build
 make -j4 bacon 2>&1 | tee build.log
 
 echo -e ""
